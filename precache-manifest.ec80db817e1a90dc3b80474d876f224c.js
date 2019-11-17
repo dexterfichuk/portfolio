82499d942e052d8dc606f4c693a908efa46747ff
Dexter Fichuk <dexter.fichuk@shopify.com>
2019-11-17 17:04:10 -0500

    Updates

---
+++ precache-manifest.ec80db817e1a90dc3b80474d876f224c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb165d137f2bdbaf6e6c6060638937e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3740eff28c1f565a184",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "632b1acbbe6e7f347b0f",
+    "url": "/portfolio/static/css/main.12039c13.chunk.css"
+  },
+  {
+    "revision": "f3740eff28c1f565a184",
+    "url": "/portfolio/static/js/2.ad971b66.chunk.js"
+  },
+  {
+    "revision": "632b1acbbe6e7f347b0f",
+    "url": "/portfolio/static/js/main.7eeddec8.chunk.js"
+  },
+  {
+    "revision": "d754ed8019fe38c19e57",
+    "url": "/portfolio/static/js/runtime-main.c1be4b81.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
